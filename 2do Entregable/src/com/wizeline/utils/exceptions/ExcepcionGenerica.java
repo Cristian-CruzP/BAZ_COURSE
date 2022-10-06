c68faf863bcb7e097369e711ed651b34cff505a4
KikeCode <86624583+KikeCode@users.noreply.github.com>
2022-10-06 16:27:42 -0500

    Update ExcepcionGenerica.java
    Se agrega clase interna

--- 2do Entregable/src/com/wizeline/utils/exceptions/ExcepcionGenerica.java
+++ 2do Entregable/src/com/wizeline/utils/exceptions/ExcepcionGenerica.java
@@ -9,8 +9,19 @@ package com.wizeline.utils.exceptions;
  */
 @SuppressWarnings("serial")
 public class ExcepcionGenerica extends RuntimeException {
+	private String mensajeError;
+	private String ok = "Peticion Correcta";
+	private String err = "Reintentar";
+	
 	public ExcepcionGenerica(String mensajeError) {
 		super(mensajeError);
 	}
 	
+	class ClaseInterna{
+		@SuppressWarnings("unused")
+		void valida() {
+			String resultado;
+			resultado = mensajeError == null ? ok: err;
+		}
+	}	
 }
